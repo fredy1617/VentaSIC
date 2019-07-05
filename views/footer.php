9e63c42238982fe228de6fa89349a7c733144939
fredy1617 <alfredomartinez6510@gmail.com>
2019-07-05 14:00:28 -0500

    modificacion mision vision
    se cambiaron por los nuevos

--- views/footer.php
+++ views/footer.php
@@ -3,11 +3,11 @@
             <div class="row">
               <div class="col l6 s12">
                 <h5 class="white-text">Misión</h5>
-                <p class="grey-text text-lighten-4">Somos una empresa dedicada a la venta y reparacion de equipo de cómputo, venta de accesorios y servicio de redes e internet a comunidades, brindando un servicio de calidad y asesoria con personal capacitado.</p>
+                <p class="grey-text text-lighten-4">Ofrecer tecnología y comunicación al alcance de todos, brindando un servicio de calidad con los mejores precios, servicios y procesos para satisfacer las necesidades de nuestros clientes.</p>
                 <h5 class="white-text">Visión</h5>
-                <p class="grey-text text-lighten-4">Ofrecer un servicio de calidad a nuestros clientes de acuerdo a los requerimientos planeados. Ser la empresa líder en la venta de computadoras portátiles y de escritorio con las mejores marcas del mercado; así como también en el servicio de redes e internet, para lo cual trabajaremos con estándares de calidad bien ejecutados para que los clientes nos prefieran.</p>
+                <p class="grey-text text-lighten-4">Ser la empresa líder en tecnología y comunicación reconocida y preferida por su calidad e innovación.</p>
               </div>
-              <div class="col l4 offset-l2 s12"><br><br><br>
+              <div class="col l4 offset-l2 s12"><br>
                 <h5 class="white-text" id="Contacto">Contactos:</h5>
                 <ul>
                   <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/SerIntCompu.SIC" target="_blank"><i class="fa fa-facebook"></i>Facebook: Servicios integrales de computación</a></li>
